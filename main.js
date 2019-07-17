6b9194c92444b22acd4d2e4b00065f0bb7b1fd5e
PaleHazy <austinrpg@live.com>
2019-07-17 11:17:18 -0500

    some changes

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-var question1 = 'Where is the Eiffel Tower located?';
-var question2 = 'How many fingers does one hand have?';
-var question3 = 'What is the meaning of life?';
+var question1 = "Where is the Eiffel Tower located?";
+var question2 = "How many fingers does one hand have?";
+var question3 = "What is the meaning of life?";
 var questions = [question1, question2, question3];
 var answers = [
-  { a: 'italy', b: 'france', c: 'germany', d: 'us' },
-  { a: 'france' }
+  { a: "Italy", b: "France", c: "Germany", d: "United States" },
+  { a: "five", b: "two", c: "one", d: "none" }
 ];
+// can set all right naswers to a : ... and then while appending set them to random
 var interval;
+var qc = document.getElementById("questionContainer");
+var ac = document.getElementById("answersContainer");
 
-interval = setInterval(creator, 5000);
+interval = setInterval(creator, 50000);
 var i = 0;
 
 function randomizer() {
@@ -21,31 +24,46 @@ function randomizer() {
   return question;
 }
 function removeQuestion() {
-  document.getElementById('questionContainer').innerHTML = '';
+  qc.innerHTML = "";
 }
 function setQuestion() {
-  document.getElementById('questionContainer').innerHTML = questions[i];
+  qc.innerHTML = questions[i];
   console.log(this.question);
 }
-function questionAnswers() {
+function addAnswers() {
   var answersObj = answers[i];
   for (var j in answersObj) {
+    var answrCont = document.createElement("div");
+    answrCont.setAttribute("class", "miniAnswerContainer");
+    var newBtn = document.createElement("button");
+    var newTxt = document.createElement("div");
+    var value = answersObj[j];
+    console.log("value:", value);
+    newBtn.setAttribute("class", "cta");
+    newTxt.setAttribute("class", "buttonText");
+    newTxt.innerHTML = value;
+    newBtn.innerHTML = j;
+    answrCont.appendChild(newBtn);
+    answrCont.appendChild(newTxt);
+    ac.appendChild(answrCont);
+    console.log("j:", j);
     //build html element per answer
     // append the html with new element
-
-
+  }
 }
-  document.getElementById('answersContainer').innerHTML = ;
-  
-  // if (this.question === question1) {
-  //   var arrayofAnswers1 = ['France'];
-  // }
+function removeAnswers() {
+  while (ac.firstChild) {
+    ac.removeChild(ac.firstChild);
+  }
 }
 //var keys = Object.keys([objectHere])
-//sample val: ["a", "b", "c", "d"] 
+//sample val: ["a", "b", "c", "d"]
 function creator() {
   removeQuestion();
   setQuestion();
-  questionAnswers();
+  removeAnswers();
+  addAnswers();
+
+  i++;
 }
 creator();
