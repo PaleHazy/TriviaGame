3e42fe13d8b84a19cec2805377fbd07818f9bb47
PaleHazy <austinrpg@live.com>
2019-07-18 12:00:11 -0500

    good progress on the thing, neeed to add right and wrong

--- main.js
+++ main.js
@@ -10,16 +10,15 @@ var answers = [
 var interval;
 var qc = document.getElementById("questionContainer");
 var ac = document.getElementById("answersContainer");
-
-interval = setInterval(creator, 50000);
+var timer = 10;
 var i = 0;
+var bClick;
 
 function randomizer() {
   //var randomQuestionPicker = Math.floor(Math.random() * questions.length);
   console.log(randomQuestionPicker);
   question = questions[randomQuestionPicker];
   questions.splice(randomQuestionPicker, 1);
-  console.log(this.question);
 
   return question;
 }
@@ -38,8 +37,8 @@ function addAnswers() {
     var newBtn = document.createElement("button");
     var newTxt = document.createElement("div");
     var value = answersObj[j];
-    console.log("value:", value);
-    newBtn.setAttribute("class", "cta");
+    newBtn.classList.add("cta", j);
+
     newTxt.setAttribute("class", "buttonText");
     newTxt.innerHTML = value;
     newBtn.innerHTML = j;
@@ -47,23 +46,66 @@ function addAnswers() {
     answrCont.appendChild(newTxt);
     ac.appendChild(answrCont);
     console.log("j:", j);
+
     //build html element per answer
     // append the html with new element
   }
+}
+var buttonStart = document.getElementById("start");
+buttonStart.addEventListener("click", start);
+function start() {
+  interval = setInterval(startGame, 1000);
+  creator();
+  buttonStart.parentNode.removeChild(buttonStart);
+}
+
+function rightAnswer() {
+  clearInterval(interval);
+  var mainContainer = document.getElementById("mainContainer");
+  var divWrong = document.createElement("div");
+  divWrong.innerHTML = "WrongAnswer";
+  while (mainContainer.children.length > 0) {
+    mainContainer.removeChild(mainContainer.firstChild);
+  }
+  mainContainer.appendChild(divWrong);
 }
 function removeAnswers() {
   while (ac.firstChild) {
     ac.removeChild(ac.firstChild);
   }
 }
+function startGame() {
+  timer--;
+  document.getElementById("timeContainer").innerHTML = timer + " Seconds Left!";
+  if (timer === 0) {
+    timer = 10;
+    creator();
+  }
+}
+function clickCreator() {
+  for (z = 0; z < selector.length; z++) {
+    selector[z].firstChild.addEventListener("click", rightAnswer);
+  }
+}
+
 //var keys = Object.keys([objectHere])
 //sample val: ["a", "b", "c", "d"]
+var selector = document.getElementById("answersContainer").children;
 function creator() {
   removeQuestion();
   setQuestion();
   removeAnswers();
   addAnswers();
-
+  clickCreator();
   i++;
 }
-creator();
+
+//everytime pending response for question add a
+// key image as the outer background
+
+// everytime answer is called and you are on right wrong screen
+// add the inner container bg picture as a key picture relevant to the answer
+
+//array (machine gun magazine)
+//loop splicing at index 0 until the length of the array is equal to zero
+//is a machine gun loop
